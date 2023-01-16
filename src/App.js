dce6e8251fdd920819745d692c423b5f98444ba5
UrvishAsodariya <mrurvishpatel366@gmail.com>
2023-01-16 10:40:52 +0530

    add bmi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function BMICalc() {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+  const calcBMI = () => {
+    setBMI(weight / (height * height));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='mainbox'>
+        <label className='text'>Height (cm):</label>
+        <input type="number" value={height} onChange={(e) => setHeight(e.target.value)} className='intbox' />
+
+        <br />
+
+        <label className='text'>Weight (kg):</label>
+        <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} className='intbox' />
+    
+        <br />
+
+        <button onClick={calcBMI} className='bttn'>Click Here </button>
+
+        <br />
+
+        <label className='text2'>Your BMI: {bmi.toFixed(2)}</label>
+      </div>
+
+
+    </>
   );
 }
 
-export default App;
+export default BMICalc;
+
